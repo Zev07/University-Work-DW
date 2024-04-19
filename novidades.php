cd68be78cd16b68c1c22376edb7e24a1f08deb9b
Zev <90863634+Zev07@users.noreply.github.com>
2024-04-19 20:17:57 -0300

    Update novidades.php

--- novidades.php
+++ novidades.php
@@ -2,7 +2,7 @@
   $hostname = "localhost";
   $bancodedados = "empresa_redbull";
   $usuario = "root";
-  $senha = "Dw-4-PJ-27!";
+  $senha = "root";
   
   $conn = new mysqli($hostname , $usuario,  $senha, $bancodedados);
 
