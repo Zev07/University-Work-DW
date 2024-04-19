62c31116e06b659080a0a37cd187b00a9cf11587
Zev <90863634+Zev07@users.noreply.github.com>
2024-04-19 15:50:50 -0300

    Update produtos.php

--- produtos.php
+++ produtos.php
@@ -1,6 +1,7 @@
+
 <?php 
   $hostname = "localhost";
-  $bancodedados = "empresa_redbull";
+  $bancodedados = "redbull";
   $usuario = "root";
   $senha = "";
   
@@ -35,58 +36,35 @@
     <title>Document</title>
 </head>
 <body>
-<header>
-        <nav class="navbar navbar-expand-lg bg-body-tertiary bg-dark ">
-            <div class="container-fluid">
-              <img class="nav-logo"src="./css/assents/img/logo.png" alt="Logotipo da Red Bull" height="80">
-              <button class="navbar-toggler" type="button"  aria-controls="navbarNav" aria-expanded="false"                   data-bs-toggle="offcanvas" data-bs-target="#offcanvasRight"  >
-                <span class="navbar-toggler-icon"></span>
-                </button>
-              <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasRight" aria-                                 labelledby="offcanvasRightLabel">
-                <div class="offcanvas-header">
-                  <ul>
-                    <li><a class="nav-link-lat" href="./home.html" >Home</a></li>
-                    <li><a class="nav-link-lat" href="./sobre.html" >Sobre</a></li>
-                    <li><a class="nav-link-lat" href="./produtos.php">Produtos </a></li>
-                    <li><a class="nav-link-lat" href="./novidades.php"> Novidades</a></li>
-                    <li><a class="nav-link-lat" href="./contato.html"> Contato</a></li>
-                  </ul>
-                  <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close">         n                   </button>
-                </div>
-              <div class="collapse navbar-collapse" id="navbarNavDropdown">
-                <ul class="navbar-nav">
-                  <li class="nav-item">
-                    <a class="nav-link" href="./home.html"> Home</a>
-                  </li>
-                  <li class="nav-item">
-                    <a class="nav-link" href="./sobre.html"> Sobre</a>
-                  </li>
-                  <li class="nav-item">
-                    <a class="nav-link" href="./produtos.php">Produtos</a>
-                  </li>
-                  <li class="nav-item">
-                    <a class="nav-link" href="./novidades.php">Novidedades</a>
-                  </li>
-                    <li class="nav-item">
-                      <a class="nav-link" href="./contato.html"> Contato</a>
-                    </li>
-                  </li>
-                </ul>   
-              </div>
-            </div>
-          </nav>    
-    </header>
+  <header>
+    <nav class="navbar navbar-expand-lg bg-body-tertiary bg-dark ">
+        <div class="container-fluid">
+          <img class="nav-logo"src="./css/assents/img/logo.png" alt="Logotipo da Red Bull" height="80">
+          
+          <div class="collapse navbar-collapse" id="navbarNavDropdown">
+            <ul class="navbar-nav">
+              <li class="nav-item">
+                <a class="nav-link" aria-current="page" href="./home.html"> Home</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="./sobre.html"> Sobre</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="./produtos.php">Produtos</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="./novidades.php">Novidedades</a>
+              </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="./contato.html"> Contato</a>
+                </li>
+              </li>
+            </ul>   
+          </div>
+        </div>
+      </nav>    
+</header>
     <main>
-     <div class="parallax-sixth"></div>
-     <div class="product-bg">
-      <div class="product-text">
-        <h1>ASAS ASAS SASASSASAS</h1>
-        <p> COCAINA É MELHOR</p>
-      </div>
-       <video autoplay muted loop>
-        <source src="./css/assents/videos/Product-Bg.mp4" type="video/mp4">
-        </video>
-       </div>
     <div class="product-container">
       <div class="product-item1">
       <?php
@@ -99,7 +77,7 @@
       // Exibir os detalhes do produto
       $row = $result->fetch_assoc();
 
-      $caminhoimagem1="css/assents/img/R-T.png";
+      $caminhoimagem1 = "css/assents/img/R-T.png";
       
       echo "<img src='$caminhoimagem1' class='product-item-sb-1'>";
       echo "<h4>" . $row["nome"] . "</h4>";
@@ -110,8 +88,171 @@
       echo "Produto não encontrado.";
       }
       ?>
+      </div>
+      <div class="product-item2">
+      <?php
+      // Consulta SQL para selecionar o ID do produto desejado
+      $sql = "SELECT id_prod, nome, valor, qtdestoque FROM produtos WHERE nome = 'RedBull Sugar Free'";
+      $result = $conn->query($sql);
+
+      // Verificar se encontrou o produto
+      if ($result->num_rows > 0) {
+      // Exibir os detalhes do produto
+      $row = $result->fetch_assoc();
+
+      $caminhoimagem2 = "css/assents/img/R-S.png";
+      
+      echo "<img src='$caminhoimagem2' class='product-item-sb-2'>";
+      echo "<h4>" . $row["nome"] . "</h4>";
+      echo "<p>Valor: R$ " . number_format($row["valor"], 2, ',', '.') . "</p>";
+      echo "<p>Disponibilidade: " . ($row["qtdestoque"] > 0 ? "Disponível" : "Indisponível") . "</p>";
+      echo "<p>ID do Produto: " . $row["id_prod"] . "</p>";
+      } else {
+      echo "Produto não encontrado.";
+      }
+      ?>
+      </div>
+      <div class="product-item3">
+      <?php
+      // Consulta SQL para selecionar o ID do produto desejado
+      $sql = "SELECT id_prod, nome, valor, qtdestoque FROM produtos WHERE nome = 'RedBull - The Açai Edition'";
+      $result = $conn->query($sql);
+
+      // Verificar se encontrou o produto
+      if ($result->num_rows > 0) {
+      // Exibir os detalhes do produto
+      $row = $result->fetch_assoc();
+
+      $caminhoimagem3 = "css/assents/img/R-A.png";
+      
+      echo "<img src='$caminhoimagem3' class='product-item-sb-3'>";
+      echo "<h4>" . $row["nome"] . "</h4>";
+      echo "<p>Valor: R$ " . number_format($row["valor"], 2, ',', '.') . "</p>";
+      echo "<p>Disponibilidade: " . ($row["qtdestoque"] > 0 ? "Disponível" : "Indisponível") . "</p>";
+      echo "<p>ID do Produto: " . $row["id_prod"] . "</p>";
+      } else {
+      echo "Produto não encontrado.";
+      }
+      ?>
+      </div>
+      <div class="product-item4">
+      <?php
+      // Consulta SQL para selecionar o ID do produto desejado
+      $sql = "SELECT id_prod, nome, valor, qtdestoque FROM produtos WHERE nome = 'RedBull - The Açai & Coco Edition'";
+      $result = $conn->query($sql);
+
+      // Verificar se encontrou o produto
+      if ($result->num_rows > 0) {
+      // Exibir os detalhes do produto
+      $row = $result->fetch_assoc();
+
+      $caminhoimagem4 = "css/assents/img/R-C.png";
+      
+      echo "<img src='$caminhoimagem4' class='product-item-sb-4'>";
+      echo "<h4>" . $row["nome"] . "</h4>";
+      echo "<p>Valor: R$ " . number_format($row["valor"], 2, ',', '.') . "</p>";
+      echo "<p>Disponibilidade: " . ($row["qtdestoque"] > 0 ? "Disponível" : "Indisponível") . "</p>";
+      echo "<p>ID do Produto: " . $row["id_prod"] . "</p>";
+      } else {
+      echo "Produto não encontrado.";
+      }
+      ?>
+      </div>
+      <div class="product-item5">
+      <?php
+      // Consulta SQL para selecionar o ID do produto desejado
+      $sql = "SELECT id_prod, nome, valor, qtdestoque FROM produtos WHERE nome = 'RedBull - The Melancia Edition'";
+      $result = $conn->query($sql);
+
+      // Verificar se encontrou o produto
+      if ($result->num_rows > 0) {
+      // Exibir os detalhes do produto
+      $row = $result->fetch_assoc();
+
+      $caminhoimagem5 = "css/assents/img/R-M.png";
+      
+      echo "<img src='$caminhoimagem5' class='product-item-sb-5'>";
+      echo "<h4>" . $row["nome"] . "</h4>";
+      echo "<p>Valor: R$ " . number_format($row["valor"], 2, ',', '.') . "</p>";
+      echo "<p>Disponibilidade: " . ($row["qtdestoque"] > 0 ? "Disponível" : "Indisponível") . "</p>";
+      echo "<p>ID do Produto: " . $row["id_prod"] . "</p>";
+      } else {
+      echo "Produto não encontrado.";
+      }
+      ?>
+      </div>
+      <div class="product-item6">
+      <?php
+      // Consulta SQL para selecionar o ID do produto desejado
+      $sql = "SELECT id_prod, nome, valor, qtdestoque FROM produtos WHERE nome = 'RedBull - The Pitaya Edition'";
+      $result = $conn->query($sql);
+
+      // Verificar se encontrou o produto
+      if ($result->num_rows > 0) {
+      // Exibir os detalhes do produto
+      $row = $result->fetch_assoc();
+
+      $caminhoimagem6 = "css/assents/img/R-P.png";
+      
+      echo "<img src='$caminhoimagem6' class='product-item-sb-6'>";
+      echo "<h4>" . $row["nome"] . "</h4>";
+      echo "<p>Valor: R$ " . number_format($row["valor"], 2, ',', '.') . "</p>";
+      echo "<p>Disponibilidade: " . ($row["qtdestoque"] > 0 ? "Disponível" : "Indisponível") . "</p>";
+      echo "<p>ID do Produto: " . $row["id_prod"] . "</p>";
+      } else {
+      echo "Produto não encontrado.";
+      }
+      ?>
+      </div>
+      <div class="product-item7">
+     
+      <?php
+      // Consulta SQL para selecionar o ID do produto desejado
+      
+      $sql = "SELECT id_prod, nome, valor, qtdestoque FROM produtos WHERE nome = 'RedBull - The Tropical Edition'";
+      $result = $conn->query($sql);
+
+      // Verificar se encontrou o produto
+      if ($result->num_rows > 0) {
+      // Exibir os detalhes do produto
+      $row = $result->fetch_assoc();
+
+      $caminhoimagem1 = "css/assents/img/R-TR.png";
+      
+      echo "<img src='$caminhoimagem1'class='product-item-sb-7'>";
+      echo "<h4>" . $row["nome"] . "</h4>";
+      echo "<p>Valor: R$ " . number_format($row["valor"], 2, ',', '.') . "</p>";
+      echo "<p>Disponibilidade: " . ($row["qtdestoque"] > 0 ? "Disponível" : "Indisponível") . "</p>";
+      echo "<p>ID do Produto: " . $row["id_prod"] . "</p>";
+      } else {
+      echo "Produto não encontrado.";
+      }
+      ?>
+      </div>
+      <div class="product-item8">
+      <?php
+      // Consulta SQL para selecionar o ID do produto desejado
+      $sql = "SELECT id_prod, nome, valor, qtdestoque FROM produtos WHERE nome = 'RedBull - The Winter Edition'";
+      $result = $conn->query($sql);
+
+      // Verificar se encontrou o produto
+      if ($result->num_rows > 0) {
+      // Exibir os detalhes do produto
+      $row = $result->fetch_assoc();
+
+      $caminhoimagem8 = "css/assents/img/R-W.png";
+      
+      echo "<img src='$caminhoimagem8' class='product-item-sb-8'>";
+      echo "<h4>" . $row["nome"] . "</h4>";
+      echo "<p>Valor: R$ " . number_format($row["valor"], 2, ',', '.') . "</p>";
+      echo "<p>Disponibilidade: " . ($row["qtdestoque"] > 0 ? "Disponível" : "Indisponível") . "</p>";
+      echo "<p>ID do Produto: " . $row["id_prod"] . "</p>";
+      } else {
+      echo "Produto não encontrado.";
+      }
+      ?>
+      </div>
     </div>
-  </div>
     </main>
     <footer></footer>
 </body>
