29eb5d75d3493ca0474e22d0b6d5ec7118fc99d3
Zev <90863634+Zev07@users.noreply.github.com>
2024-04-20 11:40:30 -0300

    Update produtos.php

--- produtos.php
+++ produtos.php
@@ -276,8 +276,8 @@
       </div>
     </div>
     </main>
-        <section class="rodape">
-      <footer class="bg-body-tertiary text-center">
+    <section class="rodape">
+      <footer id="footer-cl"class="bg-body-tertiary text-center">
         <div class="container p-4">
           <div class="row">
             <div class="col-lg-6 col-md-12 mb-4 mb-md-0">
